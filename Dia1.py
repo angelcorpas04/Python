84798cfe825f48cb8a50b71701fdeff330652fff
angelcorpas04 <145747240+angelcorpas04@users.noreply.github.com>
2023-09-22 11:08:52 +0200

    Add files via upload

---
+++ Dia1.py
@@ -0,0 +1,35 @@
+#Que bonito es Python
+print ("Hola Mundo")
+print ("Me llamo Ángel")
+#Nuestra primera variable
+x = 5
+print ("print (5):")
+print (5)       #imprime 5
+print (5+5)     #imprime 10
+print ("print (x):")
+print (x)       #5
+print ("print (x+10):")
+print (x+10)
+#Creamos una variable con texto -> string
+nombre = "Ángel"
+numero = 8
+print (nombre)
+print ("Sumo nombre + numero:")
+print ('print("Ángel"+"8")')
+print ("Ángel"+"8")
+print ('print (nombre+str(numero)):')
+print (nombre+str(numero))
+print ("Escribo 10 veces CuCu")
+print ("CuCu"*10)
+print ()
+print ("\n\\Hacemos \"operaciones\"\t matemáticas\\")
+num1 = 2
+num2 = 8
+suma = num1+num2
+resta = num2-num1
+multp = num1*num2
+div = num2/num1
+print ('suma = ' + str(suma) + ' | resta = ' + str(resta) + " | Multiplicacion = " + str(multp) + " | division = " + str(div))
+
+
+
